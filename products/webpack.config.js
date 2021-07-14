b99cc4162008bf8eaa83a4def3675235ba6ec0a4
Ashraful Islam <rx.ju.edu@gmail.com>
2021-07-14 21:23:51 +0700

    :wrench: Communication with Module Federation

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -6,6 +7,18 @@ module.exports = {
     port: 8081,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      // contains list of files from this micro frontend
+      // and directions how to load them
+      filename: "remoteEntry.js",
+      // Expose src/index
+      // Other app can use this module like => import "products/ProductsIndex";
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
+    // Will inject bundled file from dist to the following location
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
